4c3d9efd7adae25d1d72df0a36fd0d84f50ae4bf
ddururiiiiiii <o244227@daum.net>
2022-10-19 19:44:36 +0900

    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.dicount.DiscountPolicy;
 import hello.core.dicount.FixDiscountPolicy;
+import hello.core.dicount.RateDisountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -23,6 +24,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDisountPolicy();
     }
+
 }
