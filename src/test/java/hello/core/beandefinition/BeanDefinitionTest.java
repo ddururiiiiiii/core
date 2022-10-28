b5ea4ab963213cb70bf8e9f706b7c21d3d5e8c78
ddururiiiiiii <o244227@daum.net>
2022-10-28 23:49:20 +0900

    스프링 빈 설정 메타 정보 - BeanDefinition

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class BeanDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-
     @Test
     @DisplayName("빈 설정 메타정보 확인")
     void findByApplicationBean(){
