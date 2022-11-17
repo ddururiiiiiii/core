a51bcc100998b92e9cb505cd82316bcd087e5e20
ddururiiiiiii <o244227@daum.net>
2022-11-17 19:51:30 +0900

    [의존관계 자동주입] - 애노테이션 직접 만들기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
-
     @Override
     public int discount(Member member, int price) {
 
